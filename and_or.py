ad249ee15c0e481bb80c8aeb490e6ba4bf830c6b
Daniel Keyes <danieljkeyes@gmail.com>
2016-10-23 21:01:27 +0200

    fixed implementation of min has and band hash
    also made each hash matrix global, and initialized during module loading.
    
    plus various and sundry changes to parameters and comments.

--- and_or.py
+++ and_or.py
@@ -5,63 +5,55 @@ import numpy as np
 # internally compute r hashes)
 # Does that count as 1024 >= r*b + b? 1024 >= 2*r*b? 1024 >= 2*r*b + b? not sure
 # This is an approximate solution to 2*r*b = 1024 and (1/b)^(1/r) = 0.85 according to wolfram:
-r = 20
-b = 24
+r = 19
+b = 26
 
-n = 8200 # num shingles
-m = 10 # num buckets for each band
-# do these primes need to be random?
-p1 = 10007 # p1 > n.
-p2 = 1009 # p2 > m
+n = 8192 # num shingles
+m = 1007 # num buckets for each band
+# do these primes need to be random? I suspect they just need to be really big
+p1 = 1000000007 # p1 > n.
+p2 = 1000000007 # p2 > m
 shingle_hashes = None
 signature_hashes = None
 
-def init_hashes():
-    global r, b, p1, p2, shingle_hashes, signature_hashes
-    np.random.seed(12345)
-    num_hashes = r*b
-    a_params = np.random.randint(low=1, high=p1, size=(num_hashes,1))
-    b_params = np.random.randint(low=0, high=p1, size=(num_hashes,1))
-    shingle_hashes = np.concatenate([a_params, b_params], axis=1)
-    a_params = np.random.randint(low=1, high=p2, size=(num_hashes,1))
-    b_params = np.random.randint(low=0, high=p2, size=(num_hashes,1))
-    signature_hashes = np.concatenate([a_params, b_params], axis=1)
+np.random.seed(12345)
+num_hashes = r*b
+a_params = np.random.randint(low=1, high=p1, size=(num_hashes,1))
+b_params = np.random.randint(low=0, high=p1, size=(num_hashes,1))
+shingle_hashes = np.concatenate([a_params, b_params], axis=1)
+a_params = np.random.randint(low=1, high=p2, size=(num_hashes,1))
+b_params = np.random.randint(low=0, high=p2, size=(num_hashes,1))
+signature_hashes = np.concatenate([a_params, b_params], axis=1)
+
     
-def compute_hash(shingles, hashes, p, N):
+def min_hash(shingles):
+    global p1, n, shingle_hashes
+    shingles = np.asarray(shingles)
     result = []
-    for shingle in shingles:
-        min_value = np.min(((shingle * hashes[:,0] + hashes[:,1]) % p) % N)
+    for i in range(shingle_hashes.shape[0]):
+        min_value = np.min(((shingles*shingle_hashes[i,0] + shingle_hashes[i,1]) % p1) % n)
         result.append(min_value)
     return result
-
-def hash_doc(shingles):
-    global p1, n, shingle_hashes
-    return compute_hash(shingles, shingle_hashes, p1, n)
 
 def hash_bands(signature):
     global r, b, p2, m, signature_hashes
     result = []
     for i in range(b):
-        band = range(i*r, (i+1)*r)
-        h = sum(compute_hash(signature, signature_hashes[band,:], p2, m))%m
-        result.append(h)
+        h = 0
+        for j in range(i*r, (i+1)*r):
+            h += ((signature[j]*signature_hashes[j,0] + signature_hashes[j,1]) % p2) % m
+        result.append(h % m)
     return result
     
 def mapper(key, value):
-    if shingle_hashes is None:
-        init_hashes()
     # key: None
     # value: one line of input file
     tokens = value.split()
     name = int(tokens[0].split("_")[1])
     shingles = [int(i) for i in tokens[1:]]
     
-    and_hash = hash_doc(shingles)
+    and_hash = min_hash(shingles)
     or_hash = hash_bands(and_hash)
-    #print("[map] {}".format(name))
-    #print("[map] {}: {}".format(name, shingles))
-    #print("[map] and: {}".format(and_hash))
-    #print("[map] or: {}".format(or_hash))
     
     value = (name, set(shingles))
     global m
@@ -72,8 +64,6 @@ def reducer(key, values):
     check_similarity = True
     # key: key from mapper used to aggregate
     # values: list of all value for that key
-    #print("[reduce] {}: {}".format(key, values))
-    #print("[reduce] {}. checking {} documents via pairwise comparisons".format(key, len(values)))
     for (i, s1) in values:
         for (j, s2) in values:
             if i >= j:
@@ -82,9 +72,7 @@ def reducer(key, values):
                 intersection = len(s1.intersection(s2))
                 union = len(s1.union(s2))
                 similarity = float(intersection)/union
-                #print("similarity of ({},{}): {}/{} = {}".format(i, j, intersection, union, similarity))
                 if similarity >= 0.85:
                     yield i, j
             else:
                 yield i, j
-        

21eb2e409495e0515f03c32dfdd646e4275746d5
Daniel Keyes <danieljkeyes@gmail.com>
2016-10-24 22:32:14 +0200

    Change to using one set of 1024 hashes twice
    instead of two sets of 512 hashes

--- and_or.py
+++ and_or.py
@@ -12,50 +12,47 @@ if import_constants:
     b = constants.b
     p1 = constants.p1
     p2 = constants.p2
-    shingle_hashes = constants.shingle_hashes
-    signature_hashes = constants.signature_hashes
+    hashes = constants.hashes
 else:
     # Not sure how the 1024 hash-limit is checked/imposed. If we have b bands with
     # r rows each, then we need r*b shingle hashes and b signature hashes (which
     # internally compute r hashes)
     # Does that count as 1024 >= r*b + b? 1024 >= 2*r*b? 1024 >= 2*r*b + b? not sure
     # This is an approximate solution to 2*r*b = 1024 and (1/b)^(1/r) = 0.85 according to wolfram:
-    r = 23
-    b = 22
+    r = 27
+    b = 37
 
     # do these primes need to be random? I suspect they just need to be really big
     p1 = 1000000007 # p1 > n.
     p2 = 1000000007 # p2
-    shingle_hashes = None
-    signature_hashes = None
+    hashes = None
 
     np.random.seed(123)
     num_hashes = r*b
     a_params = np.random.randint(low=1, high=p1, size=(num_hashes,1))
     b_params = np.random.randint(low=0, high=p1, size=(num_hashes,1))
-    shingle_hashes = np.concatenate([a_params, b_params], axis=1)
-    a_params = np.random.randint(low=1, high=p2, size=(num_hashes,1))
-    b_params = np.random.randint(low=0, high=p2, size=(num_hashes,1))
-    signature_hashes = np.concatenate([a_params, b_params], axis=1)
+    hashes = np.concatenate([a_params, b_params], axis=1)
 
 n = 8192 # num shingles
     
 def min_hash(shingles):
-    global p1, n, shingle_hashes
+    global p1, n, hashes
     shingles = np.asarray(shingles)
     result = []
-    for i in range(shingle_hashes.shape[0]):
-        min_value = np.min(((shingles*shingle_hashes[i,0] + shingle_hashes[i,1]) % p1) % n)
+    for i in range(hashes.shape[0]):
+        min_value = np.min(((shingles*hashes[i,0] + hashes[i,1]) % p1) % n)
         result.append(min_value)
     return result
 
 def hash_bands(signature):
-    global r, b, p2, signature_hashes
+    global r, b, p2, hashes
     result = []
     for i in range(b):
         h = 0
         for j in range(i*r, (i+1)*r):
-            h += (signature[j]*signature_hashes[j,0] + signature_hashes[j,1]) % p2
+            # re-use hashes for the band hash. ideally, computing a random
+            # hash twice is still a random hash.
+            h += (signature[j]*hashes[j,0] + hashes[j,1]) % p2
         result.append(h % p2)
     return result
     
--- test_constants.py
+++ test_constants.py
@@ -4,26 +4,22 @@ import subprocess
 contents = """
 import numpy as np
 r = {}
-b = 1024 / 2 / r
+b = 1024 / r
 
 # do these primes need to be random? I suspect they just need to be really big
 p1 = 1000000007 # p1 > n.
 p2 = 1000000007 # p2
-shingle_hashes = None
-signature_hashes = None
+hashes = None
 
 np.random.seed({})
 num_hashes = r*b
 a_params = np.random.randint(low=1, high=p1, size=(num_hashes,1))
 b_params = np.random.randint(low=0, high=p1, size=(num_hashes,1))
-shingle_hashes = np.concatenate([a_params, b_params], axis=1)
-a_params = np.random.randint(low=1, high=p2, size=(num_hashes,1))
-b_params = np.random.randint(low=0, high=p2, size=(num_hashes,1))
-signature_hashes = np.concatenate([a_params, b_params], axis=1)
+hashes = np.concatenate([a_params, b_params], axis=1)
 """
 with open("out.csv", "w") as results:
-    for r in range(10,40):
-        for seed in [123, 456, 789, 37, 4, 8, 15, 16, 23, 42]:
+    for r in range(20,41):
+        for seed in range(30):
 
             with open("constants.py", "w") as f:
                 f.write(contents.format(r, seed))
@@ -45,7 +41,7 @@ with open("out.csv", "w") as results:
 
             f1 = float(out)
             
-            b = 1024 / 2 / r
-            results.write("{} {} {}, {}, {}, {}, {}, {}\n".format(r, b, tp, fp, fn, precision, recall, f1))
-            print("{}	{}	{}	{}	{}	{}	{}	{}".format(r, b, tp, fp, fn, precision, recall, f1))
+            b = 1024 / r
+            results.write("{}, {}, {}, {}, {}, {}, {}, {}\n".format(r, b, tp, fp, fn, precision, recall, f1))
+            print("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}".format(r, b, tp, fp, fn, precision, recall, f1))
 
